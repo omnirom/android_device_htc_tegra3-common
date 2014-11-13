35ab11ffc962bab8e5b36115b505f3a905db28b9
Thomas Wendt <thoemy@gmx.net>
2014-11-13 01:43:51 +0100

    tegra3-common: Update sepolicy
    Use vold domain for radio initialization. IMCdownload needs permissions to
    mount usbfs that is only allowed specific domains.
    
    Change-Id: Ia851aac3e72dcb5ab1116dd1dcf8b18cbdb356b4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -96,23 +96,25 @@ BOARD_SEPOLICY_DIRS := \
 BOARD_SEPOLICY_UNION += \
         file_contexts \
         genfs_contexts \
+	      property_contexts \
         bluetooth.te \
         btmacreader.te \
         device.te \
         domain.te \
         drmserver.te \
-	init.te \
+	      imc_download.te \
+	      init.te \
         init_shell.te \
         file.te \
         gpsd.te \
         keystore.te \
         lmkd.te \
         mediaserver.te \
+	      misc.te \
         rild.te \
-        sensors_config.te \
         surfaceflinger.te \
         system_app.te \
         system_server.te \
         ueventd.te \
-	uim_sysfs.te \
+	      uim_sysfs.te \
         vold.te
